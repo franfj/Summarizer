3460e5ffed0c450299f07abd381f306eabde4628
franfj <franjrg94@gmail.com>
2018-07-22 13:35:23 +0200

    Refactor test

--- summarizer/test/test.py
+++ summarizer/test/test.py
@@ -4,17 +4,14 @@ from summarizer.src.summarizer.summarizer import summarizer
 
 def main():
     with open("../../texts/test.txt") as f:
-        inputText = f.readlines()[0]
+        input_text = f.readlines()[0]
 
-    print inputText
-
-    summarizer.text = inputText
+    summarizer.text = input_text
     summarizer.algo = Summ.TEXT_RANK
     summarizer.percentage = 0.25
 
-    print(summarizer.summarize())
+    # print(summarizer.summarize())
     print(summarizer.schematize())
-
 
 if __name__ == "__main__":
     main()
